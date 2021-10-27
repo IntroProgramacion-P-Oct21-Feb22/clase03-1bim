424422648afd3ad14ce755661853ab8a2d46e68c
@reroes <reroes799@gmail.com>
2021-10-26 22:59:05 -0500

    ejemplos para clase 3

---
+++ ejemplos-IDE/Ejemplo07/src/ejemplo07/Ejemplo07.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejemplo07;
+
+/**
+ *
+ * @author reroes
+ */
+public class Ejemplo07 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println(true && true);
+        System.out.println(true && false);
+        System.out.println(false && false);
+        System.out.println("------------------");
+        System.out.println(true || true);
+        System.out.println(true || false);
+        System.out.println(false || false);
+        System.out.println("------------------");
+        System.out.println(!true); // negación
+        System.out.println(!false); // negación
+    }
+    
+}
